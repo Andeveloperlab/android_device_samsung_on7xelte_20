b48e9d864ac31e1e6b490b99bf88313f238d5451
Andeveloperlab <yogurtbirsah65@gmail.com>
2026-08-13 19:06:16 +0300

    on7xelte: radio: switch on7xelte

--- device.mk
+++ device.mk
@@ -236,7 +236,7 @@ PRODUCT_PACKAGES += \
     libril \
     android.hardware.radio@1.0 \
     android.hardware.radio.config@1.1-service.on7xelte \
-    android.hardware.radio@1.4-service.legacy \
+    android.hardware.radio@1.4-service.on7xelte \
     android.hardware.radio.deprecated@1.0 \
     libcutils_shims
 
